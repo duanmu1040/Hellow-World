b7130998b31c51a267937fb6b18d7adb6fb91161
Json Roll <1404463881@qq.com>
2017-02-12 21:36:29 +0800

    github使用测试

---
+++ myCode1/Console.log/Console.log/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Console.log.Startup))]
+namespace Console.log
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
